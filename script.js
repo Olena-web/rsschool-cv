8d44309a24fc0a1536343b9c6384a6eb711581e8
Olena Honcharuk <gasmin07@gmail.com>
2021-09-20 00:32:30 +0300

    feat: add links to projects

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.querySelector(".hamburger"),
     menu = document.querySelector(".menu"),
-    closeElem = document.querySelector(".menu__close");
+    closeElem = document.querySelector(".menu__close"),
+    menuSocial = document.querySelectorAll(".menu__link");
+
   hamburger.addEventListener("click", () => {
     menu.classList.add("active");
   });
   closeElem.addEventListener("click", () => {
     menu.classList.remove("active");
+  });
+  menuSocial.forEach((item) => {
+    item.addEventListener("click", () => {
+      menu.classList.remove("active");
+    });
   });
 });
